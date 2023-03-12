5d9f380d9ca3980f2a830d724897bcaff86e9f03
Muhammad Zeeshan Javed <m.zeeshan.javed@tectsoft.com>
2023-03-12 07:55:46 +0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Pages from "./routes";
+import Rap from "./pages/Rap";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <Pages />
-    </div>
+    <Rap />
   );
 }
 
---
+++ src/pages/Rap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Pages from "../routes";
+
+const Rap = () => {
+    return (
+        <>
+            <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+                <Pages />
+            </div>
+        </>
+    )
+}
+
+export default Rap
